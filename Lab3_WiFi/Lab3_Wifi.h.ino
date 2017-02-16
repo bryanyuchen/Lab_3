9088d779c4e40787cf409997c75a65e7d9bdcf2d
bryanyuchen <Bryan.Chen@Avnet.com>
2017-02-15 18:58:14 -0800

    header file

---
+++ Lab3_WiFi/Lab3_Wifi.h.ino
@@ -0,0 +1,13 @@
+/* WiFi Config Header file
+ *  
+ */
+#ifndef Lab3_WiFi
+#define Lab3_WiFi
+
+void clientSetup();
+void runClient();
+void blinkLED (int ntimes);
+void configureServer();
+void startServer();
+
+#endif
